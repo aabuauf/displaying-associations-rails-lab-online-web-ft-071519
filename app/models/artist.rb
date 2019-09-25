0db583678bd2c246ca11be69516d43e48d950d60
Ahmed Abououf <aabuauf@yahoo.com>
2019-09-25 18:06:12 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,7 +1,11 @@
 class Artist < ActiveRecord::Base
+<<<<<<< HEAD
     has_many :songs
 
     def song_count
         self.songs.count
     end
+=======
+  
+>>>>>>> 22dce6a01744e3293a45ced1521a4bfeedfb667f
 end
